2d60cbec997f9363f3b02320b3bc81b86ca2f181
Mdakszin <mdakszin@gmail.com>
2025-12-06 09:29:23 +0200

    feat: Set up core application layout and routing with React Router and various providers.

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <div className="flex flex-col min-h-screen">
             <Navigation />
             <main className="flex-grow">
